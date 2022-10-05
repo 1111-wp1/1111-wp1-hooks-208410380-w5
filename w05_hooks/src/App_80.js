9aaebf1fdd8079d289620ed66950891e86df661b
justin40715 <justin40715@gmail.com>
2022-10-05 19:04:02 +0800

    w05-P1: Hooks Tutorial T14_80

---
+++ w05_hooks/src/App_80.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import T14_80 from './tutorial/1-useState/T14_80-useState-object';
+
+const App_80 = () => {
+  return (
+    <div className='container'>
+      <h2>Tutorial 14 - 208410380</h2>
+      <T14_80 />
+    </div>
+  );
+};
+
+export default App_80;
